41cd3a64d5da177283cc25f78b2ac796f66ddfdc
VictorPedroza <victor.pedroza@protonmail.com>
2026-03-13 17:43:41 -0300

    :sparkles: Init cli on main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from core.cli.token_cli import TokenCli
+
+def main():
+    """ Função principal de Entrada """
+    cli = TokenCli()
+    cli.start()
+
+if __name__ == "__main__":
+    main()
